284ba4e2afb369572496b4828fcd8945eb33513b
Ashish Vaghela <vaghelaashishh@gmail.com>
2025-08-05 16:10:25 +0530

    Renamed folder

--- 03-Review/app.js
+++ 03-Review/app.js
@@ -1,4 +1,3 @@
-// local reviews data
 const reviews = [
   {
     id: 1,
@@ -29,7 +28,7 @@ const reviews = [
     text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
-// select items
+
 const img = document.getElementById("person-img");
 const author = document.getElementById("author");
 const job = document.getElementById("job");
@@ -39,10 +38,8 @@ const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 const randomBtn = document.querySelector(".random-btn");
 
-// set starting item
 let currentItem = 0;
 
-// load initial item
 window.addEventListener("DOMContentLoaded", function () {
   const item = reviews[currentItem];
   img.src = item.img;
@@ -51,7 +48,6 @@ window.addEventListener("DOMContentLoaded", function () {
   info.textContent = item.text;
 });
 
-// show person based on item
 function showPerson(person) {
   const item = reviews[person];
   img.src = item.img;
@@ -59,7 +55,7 @@ function showPerson(person) {
   job.textContent = item.job;
   info.textContent = item.text;
 }
-// show next person
+
 nextBtn.addEventListener("click", function () {
   currentItem++;
   if (currentItem > reviews.length - 1) {
@@ -67,7 +63,7 @@ nextBtn.addEventListener("click", function () {
   }
   showPerson(currentItem);
 });
-// show prev person
+
 prevBtn.addEventListener("click", function () {
   currentItem--;
   if (currentItem < 0) {
@@ -75,7 +71,7 @@ prevBtn.addEventListener("click", function () {
   }
   showPerson(currentItem);
 });
-// show random person
+
 randomBtn.addEventListener("click", function () {
   console.log("hello");
 
